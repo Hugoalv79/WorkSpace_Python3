4cee22b7ed5a55e10e2d2b47f4827a46bb923922
Hugo Alvarado <79931043+Sphoreas@users.noreply.github.com>
2021-08-20 09:54:04 -0500

    Add files via upload

---
+++ Boolean expressiond and comparison.py
@@ -0,0 +1,14 @@
+a = (-5.0 * 4 != 29) or (17 / 5 <= 2)
+print(a)
+
+b = ("a" != "A") and ("p" > "j")
+print(b)
+
+c = not(5 != 8) and (3.0 >= 3)
+print(c)
+
+d = (2 * 5 == 10) or (3 >= 7) and (4 <= 4)
+print(d)
+
+e = not(4 / 4 < 3) and (4 + 2 > 3)
+print(e)
