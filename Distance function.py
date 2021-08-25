7835dc2d5f2707b936e669f67e084cc7d1145cf0
Hugo Alvarado <79931043+Sphoreas@users.noreply.github.com>
2021-08-25 13:14:17 -0500

    Add files via upload

---
+++ Distance function.py
@@ -0,0 +1,13 @@
+import math
+
+def distance(a, b):
+    if (a == b):
+        return 0
+    elif (a < 0) and (b < 0) or (a > 0) and (b > 0):
+        if (a < b):
+            return (abs(abs(a) - abs(b)))
+        else:
+            return -(abs(abs(a) - abs(b)))
+    else:
+        return math.copysign((abs(a) + abs(b)),b)
+# Remember rest 2
