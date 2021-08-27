cb3585037bd23bb19ea7fd12bf8e8f0a7f76d3d8
Hugo Alvarado <79931043+Sphoreas@users.noreply.github.com>
2021-08-26 19:17:19 -0500

    Add files via upload

---
+++ Ejercicio1.py
@@ -0,0 +1,4 @@
+grados_farenheit = float(input("Ingresa los grados Farenheit: "))
+
+grados_celcius = (5/9)*(grados_farenheit-32)
+print(grados_farenheit + "equivalen a " + grados_celcius + "grados celcius")
