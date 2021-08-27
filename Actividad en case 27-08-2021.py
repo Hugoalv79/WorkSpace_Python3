f5f9e74e404ad86202cb7bbba3b2328130496a24
Hugo Alvarado <79931043+Sphoreas@users.noreply.github.com>
2021-08-27 10:31:21 -0500

    Add files via upload

---
+++ Actividad en case 27-08-2021.py
@@ -0,0 +1,38 @@
+x = 2
+y = 13
+while (x<12):
+    x = x*2
+    y +=2
+    
+print(x,y)
+
+k = 10
+r = 5
+
+while (k >= 0):
+    k = k-3
+    r = r + k // 2
+print(r,k)
+
+a = 5
+b = 3
+while (a>b):
+    b += 2
+    a += 1
+
+print(a,b)
+
+valor = 8
+while valor < 5:
+      valor = valor + 1
+
+print (valor)
+¿Qué valores se imprimen después de ejecutar lo siguiente?
+
+x = 8
+y = 6
+while y <= x :
+    x += 1
+    y += 2
+print(x)
+print(y)
