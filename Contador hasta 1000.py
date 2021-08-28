9a718e93e6272d369ccb8b4fc2b9b1a274522e1c
Hugo Alvarado <79931043+Sphoreas@users.noreply.github.com>
2021-08-28 10:12:48 -0500

    Add files via upload

---
+++ Contador hasta 1000.py
@@ -0,0 +1,17 @@
+
+
+numero = 0
+ingresados = 0
+while numero < 1000:
+    ingreso = float(input("Ingresa los números: "))
+    numero += ingreso
+    ingresados += 1
+
+else:
+    None
+
+print("La cantidad de cifras ingresadas es: ",str(ingresados))
+
+
+
+
