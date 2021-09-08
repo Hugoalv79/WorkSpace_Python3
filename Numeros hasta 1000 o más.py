8404793f7702d87d6b00d5a621282b6bcb27de6e
Hugo Alvarado <79931043+Sphoreas@users.noreply.github.com>
2021-09-08 12:59:39 -0500

    Add files via upload

---
+++ Numeros hasta 1000 o más.py
@@ -0,0 +1,10 @@
+resultado = 1
+print('Ingresa numeros hasta llegar a 1000 o mas')
+while resultado < 1000:
+    resultado *= int(input())
+    print(resultado)
+
+#Da clic en Debug currente script, bicho verde
+#Da clic en step into(F7) varias veces
+#¿Qué observaste? El código provoca un bucle infinito
+# Debes corregir el código
