da2e001d52cdaeb2013a66098c7be1d1cd51b970
Hugo Alvarado <79931043+Sphoreas@users.noreply.github.com>
2021-09-09 01:23:16 -0500

    Add files via upload

---
+++ Total y valor.py
@@ -0,0 +1,11 @@
+# Da clic en Debug currente script, bicho verde
+# Da clic en step into(F7) varias veces
+
+total = 0
+valor = 1
+while total <= 100:
+    total += int(input('Agrega un valor a total: '))
+print(total)
+
+# ¿Qué debemos hacer para corregir el código? Cambiar valor por total
+# Haz la corrección en el código
