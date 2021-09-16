e836307cb29b1f4a2f721b7b4493d442115ba2d0
Hugo Alvarado <79931043+Sphoreas@users.noreply.github.com>
2021-09-16 03:31:02 -0500

    Add files via upload

---
+++ Números recursivos descendentes.py
@@ -0,0 +1,25 @@
+# Imprimir numeros de 5 a 1 de manera descendente usando funciones recursivas. Puede ser cualquier valor positivo, ejemplo, si pasamos el valor 
+# de 5, debe imprimir:
+
+# 5
+# 4
+# 3
+# 2
+# 1
+
+# Si se pasa el valor de 3, debe imprimir:
+
+# 3
+# 2
+# 1
+
+# Si se pasan valores negativos no imprime nada
+
+def num_descendente(numero):
+    if numero >= 1:
+
+        print(numero)
+        num_descendente(numero-1)
+
+numero = int(input("Ingresa el número: "))
+num_descendente(numero)
