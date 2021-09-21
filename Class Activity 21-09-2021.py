473a12839ea3d7255f5b711996c817d2b7055859
Hugo Alvarado <79931043+Sphoreas@users.noreply.github.com>
2021-09-21 17:40:22 -0500

    Add files via upload

---
+++ Class Activity 21-09-2021.py
@@ -0,0 +1,33 @@
+# Cual es el resultado que se imprime en pantalla al ejecutar la siguiente función
+
+# def cuenta(a,b):
+#         resultado = (a **3)+ b
+#         return resultado
+# a = 3
+# b = 100
+# print(cuenta(a,b))
+
+# def uno (a, b) :
+#     a = a // 3
+#     return a + b
+
+# def main():
+#     x = 10
+#     y = 40
+#     z = uno(y, x)
+#     print(z)
+
+# main()
+
+def calculo(x, y):
+    z = x + y
+    return z
+
+def main():
+    p = 5
+    q = 2
+    r = 10
+    r = calculo(p,q)
+    print(r)
+
+main()
