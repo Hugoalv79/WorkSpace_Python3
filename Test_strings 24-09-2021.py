259e97de9d1faeb03ce3b287017109c07f4c159e
Hugo Alvarado <79931043+Sphoreas@users.noreply.github.com>
2021-09-24 16:15:11 -0500

    Add files via upload

---
+++ Test_strings 24-09-2021.py
@@ -0,0 +1,17 @@
+
+
+# ¿Qué imprime el  siguiente segmento de código?
+
+listaB = [1, 3, 5, 7, 'Hola', 'Adiós']
+x = listaB[ len(listaB) - 4 ]
+print(x)
+
+
+
+# La instrucción:
+
+list('Hola')
+
+# genera la lista ['H', 'o', 'l', 'a']?
+
+print(list)
