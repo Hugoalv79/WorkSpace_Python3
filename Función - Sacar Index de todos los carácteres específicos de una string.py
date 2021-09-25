2d9ffa7be34f1deab7ddc4a98263175b871cc119
Hugo Alvarado <79931043+Sphoreas@users.noreply.github.com>
2021-09-25 18:45:28 -0500

    Add files via upload

---
+++ Función - Sacar Index de todos los carácteres específicos de una string.py
@@ -0,0 +1,7 @@
+# if 1<= T and T<= 100:   
+#     for element in Casos:
+#         lst = [] #Indices de donde están las B
+#         for pos,char in enumerate(element):
+#             if(char == "B"):
+#                 lst.append(pos)
+#         print(lst)
