822b674e3c22432e3d404b647f07bc8e6b4a9c8f
Hugo Alvarado <79931043+Sphoreas@users.noreply.github.com>
2021-09-25 23:57:45 -0500

    Add files via upload

---
+++ Fechas 25-09-2021.py
@@ -0,0 +1,6 @@
+# Dada una fecha pasada, determinar el día de la semana al que corresponde (Lunes,...,DOmingo) y el número de días transcurridos
+# hasta el presente). Por ejemplo, el 9 de Agosto de 2010. día 
+T = input()
+T = T.split(" ")
+
+print(T)
